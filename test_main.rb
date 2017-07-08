71bf2575dbd5d073e1e90eab2c705d9f0c7e8a3a
Elinaldo Monteiro <elinaldo.java@gmail.com>
2017-07-08 00:13:27 -0300

    format

--- test_main.rb
+++ test_main.rb
@@ -3,8 +3,8 @@ require 'test/unit'
 require_relative 'main'
 
 class TestMultiple < Test::Unit::TestCase
-  def test_mult_interval
+	def test_mult_interval
 		expected = "1, 2, 3, 4, Nama, 6, Team, 8, 9, Nama, 11, 12, 13, Team, Nama, 16, 17, 18, 19, Nama, Team, 22, 23, 24, Nama, 26, 27, Team, 29, Nama, 31, 32, 33, 34, Nama Team, 36, 37, 38, 39, Nama, 41, Team, 43, 44, Nama, 46, 47, 48, Team, Nama, 51, 52, 53, 54, Nama, Team, 57, 58, 59, Nama, 61, 62, Team, 64, Nama, 66, 67, 68, 69, Nama Team, 71, 72, 73, 74, Nama, 76, Team, 78, 79, Nama, 81, 82, 83, Team, Nama, 86, 87, 88, 89, Nama, Team, 92, 93, 94, Nama, 96, 97, Team, 99, Nama"
-  	assert_equal expected, mult_interval
+    assert_equal expected, mult_interval
   end
 end
