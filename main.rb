decc6195c06bdf5774bbac789653d12ce5234d41
Elinaldo Nascimento <elinaldo.java@gmail.com>
2017-07-08 00:09:20 -0300

    first commit

---
+++ main.rb
@@ -0,0 +1,19 @@
+# encoding: utf-8
+def mult_interval
+	lst = []
+	(1..100).each do |n|
+		if n % 35 == 0
+			lst.push('Nama Team')
+		elsif n % 7 == 0
+			lst.push('Team')
+		elsif n % 5 == 0
+			lst.push('Nama')
+		else
+			lst.push(n)
+		end
+	end
+	lst.join(', ').encode("UTF-8")
+end
+
+
+puts mult_interval()
